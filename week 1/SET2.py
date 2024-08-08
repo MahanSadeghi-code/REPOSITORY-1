a472c3408542269c5b420feba016e2f832aeb162
Mahan Sadeghi <patreeick@gmail.com>
2024-08-08 14:36:58 +0330

    Add files via upload

---
+++ week 1/SET2.py
@@ -0,0 +1,45 @@
+from math import *
+
+ tamrin1 = "1   2  3  4   5"
+print (tamrin1)
+
+
+# TAMRIN 2
+
+x1 = int(input("enter x1"))
+x2 = int(input("enter x2"))
+y1 = int(input("enter y1"))
+y2 = int(input("enter y2"))
+
+print(sqrt((x2 - x1) **2 + (y2 - y1) **2 ))
+
+
+
+# TAMRIN 3
+f = int(input("hoghoogh saaty"))  
+h = int(input("saat"))
+l = f - h - 10%
+print(l)
+
+# TAMRIN 4
+g = int(input("first n"))
+d = int(input("second n"))
+b = sqrt((g*2 - g*1)**2 + (d*2 - d*1)**2)
+
+#TAMRIN 5
+x = x1 = int(input("enter 1"))
+y = int(input("enter 2"))
+z = x**23 + 2*x**2 + 3*y - 5
+
+# TAMRIN 7
+benzin = int(input("enter liter"))
+masafat = int(input("enter mile"))
+zarb = benzin * masafat
+print (zarb)
+
+
+# TAMRIN 8
+x = x1 = int(input("enter 1"))
+y = int(input("enter 2"))
+z = x ** 23 + 2 * x ** 2 + 3 * y - 5
+print(z)
