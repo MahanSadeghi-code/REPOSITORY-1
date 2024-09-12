07e7f89662d24019c834e0473bb1f2e661710c3d
Mahan Sadeghi <patreeick@gmail.com>
2024-09-12 14:09:01 +0330

    Add files via upload

---
+++ week2/bmi.py
@@ -0,0 +1,4 @@
+s = int(input("enter weight"))
+a = int(input("enter height"))
+
+d = s // a**2
---
+++ week2/iftamrin.py
@@ -0,0 +1,66 @@
+# ------end will make your sentences stick in one line
+
+print("mahan is the best student ",end="/ ")
+print("Ali is good boy")
+
+for i in range(5):   #i=0 , i=1 , i = 2 , i = 3 , 1 = 4    
+    print(i,end=" ")  
+
+
+# ------range(start stop step)
+
+# ------example
+
+
+    for i in range(2,20,2): #i = 2 4 6 8 10 12 14 16 18
+    print(i)
+
+
+# ------we can use things other than i like j
+
+
+    for j in range(4,12,5):  #j=9  #(start , end - 1 , step)
+    print(j , end = ' ' )
+
+
+# ------THErE IS A NEW WAY OF SHOWCASING WORLDS PICE BY PIECE
+
+text = "Mahan"
+
+# print(text[0])
+# print(text[1])
+# print(text[2])
+# print(text[3])
+# print(text[4])
+# print(text)
+# character
+
+text = 'amin'
+
+for ch in text:    #ch = "m"
+    print(ch,end=" ,") # a,m,i,n
+
+
+# ------other examples
+
+text = "iran"
+
+for i in text: # i = "i"
+    print(i,end="%")
+
+
+# ------other loops 
+
+for a in range(5): #ch = 0
+    print(ch)
+    print(ch * 2)
+    print( 2 * 5 )
+
+# ------list loops
+
+fruits = ["apple", "banana", "cherry"]
+
+for i in fruits: # i = "apple" ,  "banana"
+  print(i)
+
+
