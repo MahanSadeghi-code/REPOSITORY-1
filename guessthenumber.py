c927b4ba27f17d45394de43bd2b73215fe9da027
Mahan Sadeghi <patreeick@gmail.com>
2024-10-31 09:56:34 +0330

    Add files via upload

---
+++ guessthenumber.py
@@ -0,0 +1,9 @@
+n = 50
+a = int(input("enter number"))
+if a > n :
+    print(" addad shoma bozorg ast :O ")
+elif a < n :
+    print (" addad shoma koochick ast :( ")
+elif a == n :
+    print ("shoma barande shodid horaa :) ")
+    
